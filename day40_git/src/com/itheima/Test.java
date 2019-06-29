e8f309db15131d78a42a452a977318e741d4046f
hhgg <ddxvvx>
2019-06-29 23:47:02 +0800

    把本地工程添加到git

---
+++ day40_git/src/com/itheima/Test.java
@@ -0,0 +1,34 @@
+package com.itheima;
+/*
+
+            需求:
+                
+
+*/
+
+
+/**
+ * Class: Test
+ *
+ * @Author: QWJ
+ * @Date: 2019/6/29 23:17
+ */
+
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello  git ");
+        System.out.println("hello  git ");
+        System.out.println("hello  git ");
+        System.out.println("hello  git ");
+    }
+   public void test(){
+        int a=1;
+        a+=2;
+       System.out.println(a);
+   }
+
+}
