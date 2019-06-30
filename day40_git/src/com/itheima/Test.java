7092c25d6cd207be2fb38e35d9edb15f884d421a
hhgg <ddxvvx>
2019-06-30 08:42:18 +0800

    修改git工程

--- day40_git/src/com/itheima/Test.java
+++ day40_git/src/com/itheima/Test.java
@@ -20,15 +20,17 @@ public class Test {
 
     public static void main(String[] args) {
 
+        System.out.println();
         System.out.println("hello  git ");
         System.out.println("hello  git ");
         System.out.println("hello  git ");
-        System.out.println("hello  git ");
+        System.out.println("秦文菁");
     }
    public void test(){
         int a=1;
         a+=2;
        System.out.println(a);
+       System.out.println("hello");
    }
 
 }
