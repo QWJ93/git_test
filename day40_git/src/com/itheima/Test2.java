8fb5eaedc4038dcdd01462d2e3e865bc0add30c8
hhgg <ddxvvx>
2019-06-30 09:01:38 +0800

    ]
    添加test2到bend'[u

---
+++ day40_git/src/com/itheima/Test2.java
@@ -0,0 +1,25 @@
+package com.itheima;
+/*
+
+            需求:
+                
+
+*/
+
+
+/**
+ * Class: Test2
+ *
+ * @Author: QWJ
+ * @Date: 2019/6/30 8:59
+ */
+
+
+public class Test2 {
+    public void test(){
+
+        System.out.println("world.....");
+    }
+
+
+}
